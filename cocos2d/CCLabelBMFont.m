cd8307fe6249c2779cfb370c1ec5a186bffe339c
Gilad Gurantz <gilad@pb>
2012-10-15 11:48:09 -0700

    Don't crash on bmfont

--- cocos2d/CCLabelBMFont.m
+++ cocos2d/CCLabelBMFont.m
@@ -750,7 +750,10 @@ typedef struct _FontDefHashElement
 		// unichar is a short, and an int is needed on HASH_FIND_INT
 		NSUInteger key = (NSUInteger)c;
 		HASH_FIND_INT(configuration_->fontDefDictionary_ , &key, element);
-		NSAssert(element, @"FontDefinition could not be found!");
+//		NSAssert(element, @"FontDefinition could not be found!");
+        if (!element) {
+            continue;
+        }
 
 		ccBMFontDef fontDef = element->fontDef;
 
